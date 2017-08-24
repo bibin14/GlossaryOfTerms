caf81420744e9ab2a2b1766144007a88012a54c6
bibin14 <bibin14@gmail.com>
2017-08-24 16:36:50 +1000

    Home  Controller and Index View added.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using GlossaryOfTerms.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GlossaryOfTerms.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetGlossaries()
+        {
+            using (CSE_DatabaseEntities dc = new CSE_DatabaseEntities())
+            {
+                var glossaries = dc.Glossaries.OrderBy(a => a.Term).ToList();
+                return Json(new { data = glossaries }, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
