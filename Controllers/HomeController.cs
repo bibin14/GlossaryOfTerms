0c2fd1d1ec5da774c2b57e22670eb7fcb361eb02
bibin14 <bibin14@gmail.com>
2017-08-25 12:44:37 +1000

    Save and Delete Views added.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,5 +23,83 @@ namespace GlossaryOfTerms.Controllers
                 return Json(new { data = glossaries }, JsonRequestBehavior.AllowGet);
             }
         }
+
+
+        [HttpGet]
+        public ActionResult Save(int id)
+        {
+            using (CSE_DatabaseEntities dc = new CSE_DatabaseEntities())
+            {
+                var glossary = dc.Glossaries.Where(a => a.ID == id).FirstOrDefault();
+                return View(glossary);
+            }
+
+        }
+
+        [HttpPost]
+        public ActionResult Save(Glossary gloss)
+        {
+            if (ModelState.IsValid)
+            {
+                using (CSE_DatabaseEntities dc = new CSE_DatabaseEntities())
+                {
+                    if (gloss.ID > 0)
+                    {
+                        //Edit
+                        var glossary = dc.Glossaries.Where(a => a.ID == gloss.ID).FirstOrDefault();
+                        if (glossary != null)
+                        {
+                            glossary.Term = gloss.Term;
+                            glossary.Definition = gloss.Definition;
+                        }
+                    }
+                    else
+                    {
+                        //Save
+                        dc.Glossaries.Add(gloss);
+
+                    }
+
+                    dc.SaveChanges();
+                }
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            using (CSE_DatabaseEntities dc = new CSE_DatabaseEntities())
+            {
+                var glossary = dc.Glossaries.Where(a => a.ID == id).FirstOrDefault();
+                if (glossary != null)
+                {
+                    return View(glossary);
+                }
+                else
+                {
+                    return HttpNotFound();
+                }
+            }
+
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeleteGlossary(int id)
+        {
+            using (CSE_DatabaseEntities dc = new CSE_DatabaseEntities())
+            {
+                var glossary = dc.Glossaries.Where(a => a.ID == id).FirstOrDefault();
+                if (glossary != null)
+                {
+                    dc.Glossaries.Remove(glossary);
+                    dc.SaveChanges();
+                }
+                return RedirectToAction("Index", "Home");
+            }
+
+        }
     }
 }
